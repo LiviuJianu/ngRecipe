afad7cc03f5203874376cd7574f171f37c5654c7
Liviu Jianu <liviu.jianu@gmail.com>
2018-02-17 19:16:59 +0200

    get the form values from selected element in html using ViewChild
    after passing the values from html to the ts file, properly set the
    loaded values to the form's input fields

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.module';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
@@ -10,6 +10,8 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
+
+  @ViewChild('editForm') shoppingListForm: NgForm;
 
   subscription: Subscription;
   editMode = false;
@@ -24,6 +26,10 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         this.editedItemIndex = index;
         this.editMode = true;
         this.editedItem = this.shoppingListService.getIngredient(index);
+        this.shoppingListForm.setValue({
+          name: this.editedItem.name,
+          amount: this.editedItem.amount
+        });
       }
     );
   }
