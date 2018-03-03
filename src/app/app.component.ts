bb0da65d0f0dd74da4ab877044b3e97370016d80
Liviu Jianu <liviu.jianu@gmail.com>
2018-03-04 00:10:54 +0200

    implement functionality for signup using firebase api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBU66Cm4C77fWqppFoTfehB1gtXrVX2J5s',
+      authDomain: 'ng-recipe-book-d547d.firebaseapp.com'
+    });
+  }
 
   onNavigate(featureToNavigateTo: string) {
     this.loadedFeature = featureToNavigateTo;
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,9 @@
-export class AuthService {
+import * as firebase from 'firebase';
 
+export class AuthService {
+  signupUser(email: string, password: string) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(
+      error => console.log(error)
+    );
+  }
 }
