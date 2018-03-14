dfd5b74e225fd42a8a8d33eedee884a638980414
liviu <liviu.jianu@gmail.com>
2018-03-14 22:29:33 +0200

    clean the app module more by moving the providers to the core module
    Signed-off-by: liviu <liviu.jianu@gmail.com>

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from '../auth/auth.service';
+import { AuthGuardService } from '../auth/auth-guard.service';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @NgModule({
   declarations: [
@@ -16,6 +21,9 @@ import { AppRoutingModule } from '../app-routing.module';
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService
   ]
 })
 export class CoreModule {
