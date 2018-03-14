322e2ea54df2458915f17e92d6eb471b627b14e6
Liviu Jianu <liviu.jianu@gmail.com>
2018-03-14 23:22:12 +0200

    fixes for AOT compilation
    Signed-off-by: liviu <liviu.jianu@gmail.com>

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -47,6 +47,10 @@ export class RecipeEditComponent implements OnInit {
     this.onCancel();
   }
 
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   onAddIngredient() {
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
