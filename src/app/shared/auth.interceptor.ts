5e5cd5f0c74eef631af45306e865eb6d52f24d36
Liviu Jianu <liviu.jianu@gmail.com>
2018-03-17 00:39:46 +0200

    intercept requests and set custom parameters
    Signed-off-by: liviu <liviu.jianu@gmail.com>

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,11 +1,22 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
 
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted:', request);
-    return next.handle(request);
+
+    const copiedRequest = request.clone({
+    // headers: request.headers.append('auth', 'asadasd')
+    params: request.params.set('auth', this.authService.getToken())
+    });
+
+    return next.handle(copiedRequest);
   }
 
 }
