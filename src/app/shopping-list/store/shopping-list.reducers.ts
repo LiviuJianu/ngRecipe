2bd92660b162a64ba1051ebbfacd44431fdc9969
Liviu Jianu <liviu.jianu@gmail.com>
2018-03-27 23:00:45 +0300

    move the application to using ngrx/store
    this means that the ngrx/store manages all our application state
    
    implement an Action type and customize it with a payload
    
    Signed-off-by: liviu <liviu.jianu@gmail.com>

---
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,23 @@
+import { Ingredient } from '../../shared/ingredient.module';
+import * as ShoppingListActionsExports from './shopping-list.actions';
+
+
+const initialState = {
+  ingredients: [
+    new Ingredient('apples', 5),
+    new Ingredient('tomatos', 10)
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActionsExports.ShoppingListActions) {
+  switch (action.type) {
+    case ShoppingListActionsExports.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    default:
+      return state;
+  }
+  return state;
+}
