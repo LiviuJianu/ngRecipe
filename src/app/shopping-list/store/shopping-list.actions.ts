440898078c1ac03f08a4748d4d77a6bc1714f2a9
Liviu Jianu <liviu.jianu@gmail.com>
2018-03-28 01:00:35 +0300

    create a new ngrx action for Add Ingredients
    Signed-off-by: liviu <liviu.jianu@gmail.com>

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -2,6 +2,7 @@ import { Action } from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.module';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 
 export class AddIngredient implements Action {
   readonly type: string = ADD_INGREDIENT;
@@ -9,5 +10,11 @@ export class AddIngredient implements Action {
   constructor(public payload: Ingredient) {}
 }
 
+export class AddIngredients implements Action {
+  readonly type: string = ADD_INGREDIENTS;
 
-export type ShoppingListActions = AddIngredient;
+  constructor(public payload: Ingredient[]) {}
+}
+
+
+export type ShoppingListActions = AddIngredient | AddIngredients;
