23f572d6265af53952e7580ca6981c67ae8926d6
Liviu Jianu <liviu.jianu@gmail.com>
2018-04-05 22:43:59 +0300

    dispatch actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,24 @@
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as AuthActionsExport from './store/auth.actions';
 
 @Injectable()
 export class AuthService {
 
-  token: string;
-
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private store: Store<fromApp.AppState>) {}
 
   signupUser(email: string, password: string) {
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(
+        user => {
+          this.store.dispatch(new AuthActionsExport.Signup());
+        }
+      )
+      .catch(
       error => console.log(error)
     );
   }
@@ -19,9 +27,12 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
       response => {
+        this.store.dispatch(new AuthActionsExport.Signin());
         this.router.navigate(['/']);
         firebase.auth().currentUser.getIdToken().then(
-          (token: string) => this.token = token
+          (token: string) => {
+            this.store.dispatch(new AuthActionsExport.SetToken(token));
+          }
         );
       }
     )
@@ -30,19 +41,9 @@ export class AuthService {
     );
   }
 
-  getToken() {
-    firebase.auth().currentUser.getIdToken().then(
-      (token: string) => this.token = token
-    );
-    return this.token;
-  }
-
-  isAuthenticated() {
-    return this.token != null;
-  }
-
   logout() {
     firebase.auth().signOut();
+    this.store.dispatch(new AuthActionsExport.Logout());
     this.token = null;
   }
 }
