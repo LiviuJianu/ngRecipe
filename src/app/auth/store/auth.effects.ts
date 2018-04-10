37912f4f4aab28b42967520e3b573e784f48c74e
Liviu Jianu <liviu.jianu@gmail.com>
2018-04-10 22:16:48 +0300

    initial implementation of effects and on listening for events

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,10 +1,12 @@
 import { Actions, Effect } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
+import * as AuthActionsExport from './auth.actions';
 
 @Injectable()
 export class AuthEffects {
   @Effect()
-  authSignup;
+  authSignup = this.actions
+    .ofType(AuthActionsExport.TRY_SIGNUP);
 
     constructor(private actions: Actions) {}
 }
