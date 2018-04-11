5b20753c15d3efe156d3aa3402f941d451620af2
Liviu Jianu <liviu.jianu@gmail.com>
2018-04-11 23:58:04 +0300

    prevent unwanted behaviour when adding a new recipe

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -4,13 +4,16 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducers';
 import * as fromAuth from './store/auth.reducers';
+
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
   constructor(private store: Store<fromApp.AppState>) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.store.select('auth').map(
+    return this.store.select('auth')
+      .take(1)
+      .map(
       (authState: fromAuth.State ) => {
         return authState.authenticated;
       }
